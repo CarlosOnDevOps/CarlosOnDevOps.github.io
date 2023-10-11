ca56f3dc3fd8e1f3264d8a05cfcefcd8a5d57564
Carlos <111383078+CarlosOnDevOps@users.noreply.github.com>
2023-10-11 15:40:37 -0500

    removed old code and added new code to research.js

--- assets/js/research.js
+++ assets/js/research.js
@@ -1,98 +1,58 @@
-/*Research Details Table*/
+const aboutMeSection = document.querySelector(".about-me-section");
 
-const researchTable = document.querySelector(".main");
-
-const research = [
+const techStack = [
   {
-    title: "How TikTok Profits off Dangerous Health Trends",
-    authors:
-      "A.W. Ohlheiser",
-    conferences:
-      "",
-    researchYr: 2023,
-    citebox: "popup1",
-    image: "assets/images/research-page/inteferenceNetwork.png",
-    citation: {
-      vancouver:
-        "Qile Zhu, Wei Bi, Xiaojiang Liu, Xiyao Ma, Xiaolin Li and Dapeng Wu. A Batch Normalized Inference Network Keeps the KL Vanishing Away. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
-    },
-    abstract:
-      "Castor oil won’t dissolve cysts and tumors. Some creators on TikTok Shop are earning commissions by suggesting otherwise.",
-    absbox: "absPopup1",
+    language: "SAS",
+    color: "003366",
   },
-
   {
-    title: "A Call for More Rigor in Unsupervised Cross-lingual Learning",
-    authors:
-      "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre",
-    conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: 2020,
-    citebox: "popup2",
-    image: "assets/images/research-page/crossLingual.png",
-    citation: {
-      vancouver:
-        "Mikel Artetxe, Sebastian Ruder, Dani Yogatama, Gorka Labaka and Eneko Agirre. A Call for More Rigor in Unsupervised Cross-lingual Learning. The 58th Annual Meeting of the Association for Computational Linguistics 2020.",
-    },
-    abstract:
-      "This is currently left empty and this can be considered as a dummy data 2",
-    absbox: "absPopup2",
+    language: "SQL",
+    color: "00416A",
+  },
+  {
+    language: "Python",
+    color: "3776AB",
+  },
+  {
+    language: "R",
+    color: "276DC3",
+  },
+  {
+    language: "CSS",
+    color: "1572B6",
+  },
+  {
+    language: "HTML",
+    color: "E34F26",
+  },
+  {
+    language: "JavaScript",
+    color: "F7DF1E",
+  },
+  {
+    language: "Powershell",
+    color: "5391FE",
+  },
+  {
+    language: "DAX",
+    color: "002366",
   },
 ];
-AOS.init();
-const fillData = () => {
-  let output = "";
-  research.forEach(
-    ({
-      image,
-      title,
-      authors,
-      conferences,
-      researchYr,
-      citebox,
-      citation,
-      absbox,
-      abstract,
-    }) =>
-      (output += `
-            <tr data-aos="zoom-in-left"> 
-                <td class="imgCol"><img src="${image}" class="rImg"></td>
-                <td class = "researchTitleName">
-                    <div class="img-div">
-                        <span class="imgResponsive">
-                            <img src="${image}" class="imgRes">
-                        </span>
-                    </div>
-                    <a href="#0" class="paperTitle"> ${title} </a> 
-                    <div class = "authors"> ${authors} </div> 
-                    
-                    <div class="rConferences"> ${conferences} 
-                        <div class="researchY">${researchYr}</div>
-                    </div>
-                    
-                    <!--CITE BUTTON-->
-                    <div class="d-flex" style="margin-right:5%;">
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${absbox}" aria-expanded="false" aria-controls="${absbox}">
-                            BRIEFING
-                        </button>
-                
-                        <button class="button button-accent button-small text-right button-abstract " type="button" data-toggle="collapse" data-target="#${citebox}" aria-expanded="false" aria-controls="${citebox}">
-                            CITE
-                        </button>
-                    </div>
-                    <div id="${absbox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
-                        <div class="card-body">
-                            ${abstract}    
-                        </div>
-                    </div>
-                    <div id="${citebox}" class="collapse" aria-labelledby="headingTwo" data-parent=".collapse">
-                        <div class="card-body">
-                            ${citation.vancouver}    
-                        </div>
-                    </div>
-                </td>
-            </tr>`)
-  );
-  researchTable.innerHTML = output;
+
+const createTechBadge = ({ language, color }) => `
+  <img src="https://img.shields.io/badge/${language}-${color}?style=for-the-badge&logo=${language.toLowerCase()}&logoColor=white" alt="${language}">
+`;
+
+const fillTechStack = () => {
+  const techStackContainer = document.createElement("div");
+  techStackContainer.classList.add("tech-stack");
+
+  techStack.forEach((tech) => {
+    const badge = createTechBadge(tech);
+    techStackContainer.innerHTML += badge;
+  });
+
+  aboutMeSection.appendChild(techStackContainer);
 };
-document.addEventListener("DOMContentLoaded", fillData);
+
+document.addEventListener("DOMContentLoaded", fillTechStack);
