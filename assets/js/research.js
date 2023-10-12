ac2479d034f31311ae5d668a92fcb1ca27ed8210
Carlos <111383078+CarlosOnDevOps@users.noreply.github.com>
2023-10-12 16:54:01 -0500

    committing only the first 2 skills

--- assets/js/research.js
+++ assets/js/research.js
@@ -38,59 +38,6 @@ const research = [
       "Microsoft Excel, Python, R, SAS, SQL, Tableau, Power BI, Microsoft SQL Server, PostgreSQL, Jupyter Notebook",
     absbox: "absPopup1",
   },
-
-  {
-    title:
-      "DATA ENGINEERING",
-    authors: "Constructing the digital pipelines that make data flow like highways",
-    conferences:
-      "",
-    researchYr: "Key Attributes: Data Pipeline | Data Quality | Speed and Efficiency",
-    citebox: "popup3",
-    image: "assets/images/research-page/wordRepresentation.png",
-    citation: {
-      vancouver:
-        "Imagine being a data architect extraordinaire. You build and maintain the backbone of your company's data systems, ensuring everything runs super smoothly. Your work keeps the data flowing, making everyone else's jobs easier. You're the unsung hero behind the scenes.",
-    },
-    abstract:
-      "Apache Hadoop, Apache Spark, Amazon Redshift, Apache Airflow, Scala, Talend",
-    absbox: "absPopup3",
-  },
-  {
-    title:
-      "DATA SCIENCE",
-    authors: "It's not magic, but it's the closest thing we have to unraveling the mysteries of the universe",
-    conferences:
-      "The 58th Annual Meeting of the Association for Computational Linguistics",
-    researchYr: "Key Attributes: ",
-    citebox: "popup3",
-    image: "assets/images/research-page/wordRepresentation.png",
-    citation: {
-      vancouver:
-        "In the business world, data scientists are like fortune tellers. You use your magic (a.k.a. machine learning) to predict when things might go wrong, so your company can fix them before they do. It's like having a crystal ball for avoiding disasters and keeping everything running smoothly.",
-    },
-    abstract:
-      "Python, R, Jupyter Notebooks, Matplotlib, Seaborn, Plotly, Git, GitHub",
-    absbox: "absPopup3",
-  },
-
-  {
-    title:
-      "CLOUD COMPUTING",
-    authors: "Where data drifts like a fluffy cloud, but the possibilities are sky-high",
-    conferences:
-      "",
-    researchYr: "Key Attributes: Scalability | Accessibility | Cost-Efficiency",
-    citebox: "popup3",
-    image: "assets/images/research-page/wordRepresentation.png",
-    citation: {
-      vancouver:
-        "As a cloud pro, you have the power to deploy applications like a commander. When your company needs more resources, you just call in reinforcements from the cloud. It's like having an army of digital helpers at your beck and call, ready to conquer any IT challenge.",
-    },
-    abstract:
-      "AWS, Azure, Google Cloud, Docker, IAM, Azure Active Directory",
-    absbox: "absPopup3",
-  },
 ];
 AOS.init();
 const fillData = () => {
